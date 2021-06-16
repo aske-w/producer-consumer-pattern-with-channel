02e304cbf0deab225ba1f210e73918831d46d391
Aske Wachs <askw@itu.dk>
2021-06-16 22:16:19 +0200

    console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace producer_consumer_pattern_with_channel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
